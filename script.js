c0e33a7c02f4d9423c6752120632c844fa354644
Rafael Ribeiro <cortadordeunha666@gmail.com>
2022-01-15 20:22:27 -0300

    feat: create interactivty of the new section.

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 "use strict"
 
 const mainEL = document.querySelector('main')
-const navigationOptionsELs = Array.from(document.querySelectorAll('[data-js="navigation-list"] button'))
+
+const navigationOptionsELs = Array.from(document.querySelectorAll('[data-js="page-header__list"] button'))
 const exploreButtonEL = document.querySelector('[data-js="explore-button"]')
 
+const animationWrappersELs = Array.from(document.querySelectorAll('[data-js="animation-wrapper"]'))
+const destinationSectionImagesELs = Array.from(document.querySelectorAll('[data-js="destination-section__image"]'))
+const destinationSectionOptiosELs = Array.from(document.querySelectorAll('[data-js="destination-section__list__button"]'))
+const destinationSectionDestineELs = Array.from(document.querySelectorAll('[data-js="destination-section__destine"]'))
 
-function changeUnderlinedOption(optionToUnderlineIndex) {
-  const underlinedOption = navigationOptionsELs.find(value => value.classList.contains('page-header__option--active'))
-  const optionToUnderline = navigationOptionsELs[optionToUnderlineIndex]
+let destinationSectionCarouselIndex = 0
 
-  underlinedOption.classList.remove('page-header__option--active')
-  optionToUnderline.classList.add('page-header__option--active')
+
+function changeUnderlinedOption(optionToUnderlineIndex, elementsToAnalise) {
+  const underlinedOption = elementsToAnalise.find(value => value.classList.contains('option-button--active'))
+  const optionToUnderline = elementsToAnalise[optionToUnderlineIndex]
+
+  underlinedOption.classList.remove('option-button--active')
+  optionToUnderline.classList.add('option-button--active')
 }
 
 function changeToOtherSection(sectionToGoIndex) {
@@ -19,7 +27,32 @@ function changeToOtherSection(sectionToGoIndex) {
 
   mainEL.style.transform = `translateX(-${amountToTransform}px)`
 
-  changeUnderlinedOption(sectionToGoIndex)
+  changeUnderlinedOption(sectionToGoIndex, navigationOptionsELs)
+}
+
+function resetAnimation(value) {
+  value.classList.remove('is-animating')
+  setTimeout(() => value.classList.add('is-animating'), 50)
+}
+
+function moveCarousel(sectionToGoIndex) {
+  animationWrappersELs.forEach(resetAnimation)
+
+  setTimeout(() => {
+    destinationSectionDestineELs[destinationSectionCarouselIndex].classList.remove('is-active')
+    destinationSectionImagesELs[destinationSectionCarouselIndex].classList.remove('is-active')
+
+    destinationSectionDestineELs[sectionToGoIndex].classList.add('is-active')
+    destinationSectionImagesELs[sectionToGoIndex].classList.add('is-active')
+
+    destinationSectionCarouselIndex = sectionToGoIndex
+  }, 225)
+
+  changeUnderlinedOption(sectionToGoIndex, destinationSectionOptiosELs)
+}
+
+function getCurrentSectionIndex() {
+  return navigationOptionsELs.findIndex(value => value.classList.contains('option-button--active'))
 }
 
 
@@ -28,3 +61,9 @@ navigationOptionsELs.forEach((value, index) => {
 })
 
 exploreButtonEL.addEventListener('click', () => changeToOtherSection(1))
+
+destinationSectionOptiosELs.forEach((value, index) => {
+  value.addEventListener('click', () => moveCarousel(index))
+})
+
+window.addEventListener('resize', () => changeToOtherSection(getCurrentSectionIndex()))
